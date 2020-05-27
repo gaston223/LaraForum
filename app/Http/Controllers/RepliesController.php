e8737eac3280345b520c7b5dea54b2963e4b7f47
gaston223 <g.coulibalyy@gmail.com>
2020-05-27 21:01:12 +0200

    Envoi de Mail pour chaque réponse à une discussion

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Discussion;
 use App\Http\Requests\CreateReplyRequest;
+use App\Notifications\NewReplyAdded;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -43,6 +44,7 @@ class RepliesController extends Controller
             'discussion_id' => $discussion->id
         ]);
 
+        $discussion->author->notify(new NewReplyAdded($discussion));
         session()->flash('success' ,'Votre réponse a bien été ajoutée');
 
         return redirect()->back();
---
+++ app/Notifications/NewReplyAdded.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Discussion;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewReplyAdded extends Notification
+{
+    use Queueable;
+
+    /**
+     * La discussion qui fait l'objet d'une réponse
+     * @var Discussion
+     */
+    public $discussion;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Discussion $discussion
+     */
+    public function __construct(Discussion $discussion)
+    {
+        $this->discussion = $discussion;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Une nouvelle réponse à été ajoutée à votre discussion')
+                    ->action('Voir la discussion', route('discussions.show', $this->discussion->slug))
+                    ->line('Lara - Forum pour vous servir !');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'discussion' => $this->discussion
+        ];
+    }
+}
